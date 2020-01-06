998bee3bbc0694bb3ae2d0a5a78159dd017aeafc
wesquintana <wesquintana@gmail.com>
2020-01-06 15:42:17 -0700

    Initialize

---
+++ server/controllers/ListController.js
@@ -0,0 +1,7 @@
+import listService from "../services/ListService";
+import express from "express";
+export default class ListController {
+  constructor() {
+    this.router = express.Router();
+  }
+}
--- server/middleware/session.js
+++ server/middleware/session.js
@@ -1,26 +1,25 @@
-import expressSession from 'express-session'
+import expressSession from "express-session";
 import mongoose from "mongoose";
 var mongoStore = require("connect-mongo")(expressSession);
 
 var store = new mongoStore({
-    mongooseConnection: mongoose.connection
+  mongooseConnection: mongoose.connection
 });
 
-store.on("error", function (err) {
-    console.log("[SESSION ERROR]", err);
+store.on("error", function(err) {
+  console.log("[SESSION ERROR]", err);
 });
-
 
 export default class Session {
-    constructor() {
-        this.express = expressSession({
-            secret: "you should change this", //CHANGE ME!!!!
-            cookie: {
-                maxAge: 1000 * 60 * 60 * 24 * 7 * 52 * 2,
-            },
-            store,
-            resave: true,
-            saveUninitialized: true
-        })
-    }
+  constructor() {
+    this.express = expressSession({
+      secret: "It's a secret to everybody!", //CHANGE ME!!!!
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 52 * 2
+      },
+      store,
+      resave: true,
+      saveUninitialized: true
+    });
+  }
 }
