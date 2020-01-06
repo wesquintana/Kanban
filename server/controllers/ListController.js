9378eaea9f843a0bc45c9f4ab16c3cc1fdecf543
wesquintana <wesquintana@gmail.com>
2020-01-06 16:35:00 -0700

    can make lists post request

--- server/controllers/ListController.js
+++ server/controllers/ListController.js
@@ -1,4 +1,4 @@
-import listService from "../services/ListService";
+import _listService from "../services/ListService";
 import express from "express";
 import { Authorize } from "../middleware/authorize.js";
 
@@ -8,13 +8,22 @@ export default class ListController {
       .Router()
       .use(Authorize.authenticated)
       .post("", this.create)
-      .put("/:id", this.edit)
-      .delete("/:id", this.delete)
+      // .put("/:id", this.edit)
+      // .delete("/:id", this.delete)
       .use(this.defaultRoute);
   }
   // this is pretty neat
 
   defaultRoute(req, res, next) {
     next({ status: 404, message: "No Such Route" });
+  }
+
+  async create(req, res, next) {
+    try {
+      let data = await _listService.create(req.body);
+      return res.status(201).send(data);
+    } catch (error) {
+      next(error);
+    }
   }
 }
--- server/services/ListService.js
+++ server/services/ListService.js
@@ -1,12 +1,14 @@
-import mongoose from 'mongoose'
-import List from "../models/List"
+import mongoose from "mongoose";
+import List from "../models/List";
 
-const _repository = mongoose.model('List', List)
+const _repository = mongoose.model("List", List);
 
 class ListService {
-
+  async create(rawData) {
+    let data = await _repository.create(rawData);
+    return data;
+  }
 }
 
-
-const _listService = new ListService()
-export default _listService
+const _listService = new ListService();
+export default _listService;
