264f8aad98214f089224b3083c004f54641ca130
wesquintana <wesquintana@gmail.com>
2020-01-06 16:21:14 -0700

    sets up ListController

--- server/controllers/ListController.js
+++ server/controllers/ListController.js
@@ -1,7 +1,20 @@
 import listService from "../services/ListService";
 import express from "express";
+import { Authorize } from "../middleware/authorize.js";
+
 export default class ListController {
   constructor() {
-    this.router = express.Router();
+    this.router = express
+      .Router()
+      .use(Authorize.authenticated)
+      .post("", this.create)
+      .put("/:id", this.edit)
+      .delete("/:id", this.delete)
+      .use(this.defaultRoute);
+  }
+  // this is pretty neat
+
+  defaultRoute(req, res, next) {
+    next({ status: 404, message: "No Such Route" });
   }
 }
