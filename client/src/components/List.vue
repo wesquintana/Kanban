b1ff767ba4de07548b478e57f369149d5ca8fe94
wesquintana <wesquintana@gmail.com>
2020-01-07 14:48:46 -0700

    creates task component

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -1,14 +1,48 @@
 <template>
-  <div class="list">{{listData.title}}</div>
+  <div class="list">
+    {{listData.title}}
+    <div class="row">
+      <div class="col-12">
+        <form @submit.prevent="addTask">
+          <div class="form-group">
+            <input
+              v-model="newTask.description"
+              type="text"
+              class="form-control"
+              placeholder="task name..."
+              required
+            />
+          </div>
+          <button class="btn button btn-primary">New List</button>
+        </form>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-10 m-auto"></div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
   name: "List",
   props: ["listData"],
   data() {
-    return {};
+    return {
+      newTask: {
+        description: "",
+        listId: this.listData._id,
+        authorId: this.$store.state.user._id,
+        boardId: this.boardId
+      }
+    };
   },
-  methods: {}
+  methods: {
+    addTask() {
+      let task = { ...this.newTask };
+      this.$store.dispatch("addTask", task);
+      this.newTask.description = "";
+    }
+  }
 };
 </script>
 <style>
---
+++ client/src/components/Task.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="task">Everything Goes Here</div>
+</template>
+<script>
+export default {
+  name: "Task",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style>
+</style>
